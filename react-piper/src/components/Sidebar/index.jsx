fd41bafcde86a3d578e39e50087b0dd9c52b4cdd
David Kuryakin <dkuryakin@gmail.com>
2022-12-07 11:53:23 +0300

    fixes

--- react-piper/src/components/Sidebar/index.jsx
+++ react-piper/src/components/Sidebar/index.jsx
@@ -1,6 +1,37 @@
 import React, {useEffect, useState} from 'react';
 import {specToNodes} from "../../utils/spec";
 
+const specMock = [
+    {'func': 'read_image', 'input': {'filename': {'type': 'string'}, 'data': {'type': 'bytes'}}, 'output': {'type': 'array', 'value_type': {'type': 'tensor'}}},
+    {'func': 'find_documents', 'input': {'image': {'type': 'tensor'}}, 'output': {'type': 'array', 'value_type': {'type': 'object', 'value_type': {'image': {'type': 'tensor'}, 'coords': {'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}}}}},
+    {'func': 'enrich_list', 'input': {'items': {'type': 'array', 'value_type': {'type': 'any'}}}, 'output': {'type': 'array', 'value_type': {'type': 'dict', 'key_type': {'type': 'string'}, 'value_type': {'type': 'any'}}}},
+    {'func': 'classify_document', 'input': {'image': {'type': 'tensor'}, 'coords': {'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}}, 'output': {'type': 'object', 'value_type': {'image': {'type': 'tensor'}, 'type': {'type': 'string'}, 'rotation': {'type': 'integer'}, 'coords': {'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}, 'confidence': {'type': 'float'}}}},
+    {'func': 'find_fields', 'input': {'image': {'type': 'tensor'}, 'document_type': {'type': 'string'}, 'coords': {'type': 'union', 'value_type': [{'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}, {'type': 'none'}]}}, 'output': {'type': 'array', 'value_type': {'type': 'object', 'value_type': {'name': {'type': 'string'}, 'crops': {'type': 'array', 'value_type': {'type': 'object', 'value_type': {'image': {'type': 'tensor'}, 'coords': {'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}}}}, 'whole_crops': {'type': 'array', 'value_type': {'type': 'object', 'value_type': {'image': {'type': 'tensor'}, 'coords': {'type': 'array', 'value_type': {'type': 'tuple', 'value_type': [{'type': 'integer'}, {'type': 'integer'}]}}}}}}}}},
+    {'func': 'ocr', 'input': {'image': {'type': 'tensor'}, 'document_type': {'type': 'string'}, 'field_name': {'type': 'string'}}, 'output': {'type': 'object', 'value_type': {'text': {'type': 'string'}, 'confidence': {'type': 'float'}}}},
+    {'func': 'ocr_many', 'input': {'images': {'type': 'array', 'value_type': {'type': 'tensor'}}, 'document_type': {'type': 'string'}, 'field_name': {'type': 'string'}}, 'output': {'type': 'object', 'value_type': {'text': {'type': 'string'}, 'confidence': {'type': 'float'}}}},
+]
+
+const nodeSpec = [
+    {
+        label: 'Input',
+        type: 'input',
+        input: [],
+    },
+    {
+        label: 'Output',
+        type: 'output',
+        output: [],
+    },
+    {
+        label: 'Map',
+        type: 'group',
+        func: 'map',
+        width: 203,
+        height: 123,
+        zIndex: -1,
+    },
+]
+
 const Sidebar = ({specs_url}) => {
     const [specs, setSpecs] = useState(null);
 
@@ -10,25 +41,15 @@ const Sidebar = ({specs_url}) => {
     };
 
     useEffect(() => {
+        if (!specs_url) {
+            return [
+                ...nodeSpec,
+                ...specToNodes(specMock),
+            ]
+        }
+
         fetch(specs_url).then(res => res.json()).then(spec => setSpecs([
-            {
-                label: 'Input',
-                type: 'input',
-                input: [],
-            },
-            {
-                label: 'Output',
-                type: 'output',
-                output: [],
-            },
-            {
-                label: 'Map',
-                type: 'group',
-                func: 'map',
-                width: 203,
-                height: 123,
-                zIndex: -1,
-            },
+            ...nodeSpec,
             ...specToNodes(spec),
         ]))
     }, [setSpecs, specs_url])
